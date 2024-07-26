0e4050c3b867e332fa8b698d2360e4831958c928
KidKat <harshachakravarthy27@gmail.com>
2024-07-26 16:06:40 +0530

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,22 @@ function App() {
         <div className="container">
           <h1>Answers</h1>
           <audio controls>
-              <source src="https://github.com/KidKat27/musicplayer/raw/main/src/assets/K!dKat_Towards_infinity.mp3" type="audio/mpeg"/>
+              <source src="https://github.com/KidKat27/musicplayer/raw/main/src/assets/KidKat_Answers_(Unfinished).mp3" type="audio/mpeg"/>
             </audio>
         </div>
         <div className="container">
           <h1>Ray of Hope</h1>
           <audio controls>
-              <source src="./src/assets/KidKat_Ray_of_Hope.wav" type="audio/mpeg"/>
+              <source src="https://github.com/KidKat27/musicplayer/raw/main/src/assets/KidKat_Ray_of_Hope.wav" type="audio/mpeg"/>
             </audio>
         </div>
         <div className="container">
           <h1>Towards Infinity</h1>
           <audio controls>
-              <source src="./src/assets/K!dKat_Towards_infinity.mp3" type="audio/mpeg"/>
+              <source src="https://github.com/KidKat27/musicplayer/raw/main/src/assets/K!dKat_Towards_infinity.mp3" type="audio/mpeg"/>
             </audio>
         </div>
+        <h1 className="Suggest">*Please use earphones/ headphones</h1>
       </div>
     </>
   )
